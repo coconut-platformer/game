b58a0889cfe757d94eac77368925078f0de129cb
Alex Barry <alex.barry@gmail.com>
2019-03-28 21:39:34 -0400

    Remove old draw function in index

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ import coconutImage from "./assets/coconut.png";
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 
-const drawGround = () => {
-  context.fillStyle = "#aa0";
-  context.fillRect(0, 600, 1024, 168);
-
-  context.strokeStyle = "#000";
-  for (let x = 0; x < 1024; x += 20) {
-    context.strokeRect(x, 600, 1, 168);
-  }
-};
 
 const assetManager = new AssetManager();
 assetManager
@@ -45,10 +36,10 @@ function runGame() {
       timer.addTime(timestamp);
       const movement = timer.getDelta();
 
-      ctx.fillStyle = "#fff";
-      ctx.fillRect(0, 0, 1024, 768);
-      world.draw(ctx);
-      coconut.draw(ctx, 0);
+      context.fillStyle = "#fff";
+      context.fillRect(0, 0, 1024, 768);
+      world.draw(context);
+      coconut.draw(context);
       world.updateDrawPosition(movement * -0.2);
       requestAnimationFrame(tick);
     };
