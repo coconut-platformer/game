212c56f2b3cad137268090add0c8611bca2c30a0
fullsnack <thefullsnackdeveloper@gmail.com>
2019-04-04 19:46:05 -0400

    Adds new terrain block variants

--- src/terrain/stone.js
+++ src/terrain/stone.js
@@ -1,10 +1,12 @@
-import TerrainBlock from './terrainBlock.js';
-import Shrub from './decorations/shrub';
 import Rock from './decorations/rock';
+import Shrub from './decorations/shrub';
+import TerrainBlock from './terrainBlock.js';
 
 export default class Stone extends TerrainBlock {
   constructor(assetManager, x, y, width, height) {
-    super(assetManager.getImage('stone'), x, y, width, height);
+    const stoneNames = ['stone', 'stone-2'];
+    const stoneName = stoneNames[Math.floor(Math.random() * stoneNames.length)];
+    super(assetManager.getImage(stoneName), x, y, width, height);
     this.assetManager = assetManager;
     this.decorations = [
       new Rock(this.assetManager),
