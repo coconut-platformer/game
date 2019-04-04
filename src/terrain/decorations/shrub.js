9c4e027a1588a09b0b2fd7956e2505c760281fd5
fullsnack <thefullsnackdeveloper@gmail.com>
2019-04-03 21:28:50 -0400

    add assets and adjust placements

--- src/terrain/decorations/shrub.js
+++ src/terrain/decorations/shrub.js
@@ -1,7 +1,7 @@
 import BlockDecoration from "./blockDecoration";
 
 export default class Shrub extends BlockDecoration {
-  constructor(assetManager, xOffset = 30, yOffset = 5, scalingFactor = 0.5) {
+  constructor(assetManager, xOffset = 30, yOffset = 5, scalingFactor = 1) {
     super(assetManager.getImage("shrub"), xOffset, yOffset, scalingFactor);
   }
 }
