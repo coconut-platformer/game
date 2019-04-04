b2dab90072515f04ec80e73c012a5d826ca24eee
Alex Barry <alex.barry@gmail.com>
2019-04-04 19:48:36 -0400

    Terrain can now flag this.dangerous for the Ai and maybe other things

--- src/terrain/water.js
+++ src/terrain/water.js
@@ -2,7 +2,7 @@ import TerrainBlock from './terrainBlock.js';
 
 export default class Water extends TerrainBlock {
   constructor(assetManager, x, y, width, height) {
-    super(assetManager.getImage('water'), x, y, width, height);
+    super(assetManager.getImage('water'), x, y, width, height, true);
     this.assetManager = assetManager;
     // this.addRandomDecoration(0.1);
   }
