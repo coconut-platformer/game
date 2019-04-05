8d1fb5ba58772f4dd08ac88a6fae609266cce852
Gerrie Swart <johnblackspear@gmail.com>
2019-04-05 09:14:04 -0400

    Cleanup of PRNG

--- src/math/prng.js
+++ src/math/prng.js
@@ -4,28 +4,47 @@
  * adapted from P5
  */
 export default class LCG {
+  /**
+   * Setup values taken from 
+   * http://en.wikipedia.org/wiki/Numerical_Recipes
+   * 
+   * Modulus is chosen to be large (as it's the max period of the PRNG)
+   * as well as for its relationship to the multiplier and increment.
+   * 
+   * (multiplier - 1) should be divisible by the modulus's prime factors.
+   * 
+   * increment and modulus should be co-prime.
+   * 
+   * Other options for values:
+   * - Borland C/C++ uses a modulus of 2^32, a multiplier of 
+   *   22695477, and increment of 1.
+   * - java.util.Random uses modulus 2^48, multiplier of 25214903917, 
+   *   and increment of 11.
+   */
   constructor(seed = 1) {
-    // Set to values from http://en.wikipedia.org/wiki/Numerical_Recipes
-    // m is basically chosen to be large (as it is the max period)
-    // and for its relationships to a and c
-    this.m = 4294967296;
-    // a - 1 should be divisible by m's prime factors
-    this.a = 1664525;
-    // c and m should be co-prime
-    this.c = 1013904223;
+    this.modulus = 4294967296;
+    this.multiplier = 1664525;
+    this.increment = 1013904223;
     this.seed = seed;
     this.z = seed;
   }
 
   setSeed(seed) {
-    this.z = this.seed = (seed == null ? Math.random() * this.m : seed) >>> 0
+    this.seed = (seed === null ? Math.random() * this.modulus : seed) >>> 0;
+    this.z = this.seed;
   }
 
+  /**
+   * First define the recurrence relation
+   *   (i.e. X_n+1 = (multiplier*X_n + increment) mod modulus)
+   *   then return a float in [0, 1)
+   * 
+   * Always less than one because:
+   * if z === modulus then z % modulus = 0
+   * so (z % modulus) / modulus < 1
+   */
   next() {
-    // define the recurrence relationship
-    this.z = (this.a * this.z + this.c) % this.m;
-    // return a float in [0, 1)
-    // if z = m then z / m = 0 therefore (z % m) / m < 1 always
-    return this.z / this.m;
+    this.z = (this.multiplier * this.z + this.increment) % this.modulus;
+    return this.z / this.modulus;
   }
 }
